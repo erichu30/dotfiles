1952b9c8aeaea664912f63d8c3a607e6fd171a70
erichu30 <erichu211@icloud.com>
2022-01-05 16:59:15 +0800

    update

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ sudo apt update && sudo apt install vim git curl -y
 # cp .vimrc
 cp tools/vim/.vimrc ~/
 
-# cp vim including plugins
-cp -r tools/vim/.vim ~/
-
 # install vim Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
