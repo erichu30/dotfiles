d760906c539a0cc5904ea465460431757a34035c
erichu <erichu@erichu.cc>
2023-07-24 20:30:50 +0800

    update

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ function installPackage() {
 }
 
 function backupDotfiles() {
-	targetDotfiles=".vimrc .bashrc .tmux.conf"
+	targetDotfiles=".bashrc .vimrc .tmux.conf"
 	for i in $targetDotfiles; do
 		if [ -f "~/$i" ]; then
 			echo "backup $i"
@@ -43,7 +43,7 @@ function backupDotfiles() {
 }
 
 function recoverDotfiles() {
-	targetDotfiles=".vimrc .bashrc .tmux.conf"
+	targetDotfiles=".bashrc .vimrc .tmux.conf"
 	for i in $targetDotfiles; do
 		if [ -f "~/$i.bak" ]; then
 			echo "recover $i"
@@ -52,6 +52,111 @@ function recoverDotfiles() {
 			mv ~/$i.bak.tmp ~/$i.bak
 		fi
 	done
+}
+
+function setupBash-it() {
+	# install bash-it
+	git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+	y | ~/.bash_it/install.sh
+
+	echo "===================="
+	echo "use bash-it command to manage bash-it: "
+	echo "bash-it show aliases"
+	echo "bash-it show completion"
+	echo "bash-it show plugins"
+	echo "bash-it show themes"
+	echo "===================="
+	
+	defaultAlias="clipboard ansible curl systemd general git tmux vim docker"
+	defaultCompletion="tmux ssh pip3 pip git export docker brew"
+	defaultPlugin="docker edit-mode-vi extract git history less-pretty-cat postgres pyenv python ssh tmux virtualenv xterm z_autoenv"
+	
+	for i in $defaultApp; do
+		bash-it enable alias $i
+	done
+
+	for i in $defaultCompletion; do
+		bash-it enable completion $i
+	done
+
+	for i in $defaultPlugin; do
+		bash-it enable plugin $i
+	done
+}
+
+function setupVim() {
+	# # install vundle
+	# git clone 
+
+}
+
+DEVSHELL=""
+DEVSHELLRC=""
+function setupDevShell() {
+	# ask user to choose dev shell
+	echo "Choose your dev shell:"
+	echo "1. bash"
+	echo "2. zsh"
+	echo "3. sh"
+
+	read -p "Enter your choice: " choice
+
+	case $choice in
+		1)
+			DEVSHELL="bash"
+			DEVSHELLRC=".bashrc"
+			;;
+		2)
+			DEVSHELL="zsh"
+			DEVSHELLRC=".zshrc"
+			;;
+		3)
+			DEVSHELL="sh"
+			DEVSHELLRC=".shrc"
+			;;
+		*)
+			echo "Unknown choice"
+			exit 1
+			;;
+	esac
+}
+
+function setupTmux() {
+	# # install tpm
+	# git clone
+
+	# # install tmux plugins
+	# tmux source-file ~/.tmux.conf
+
+	# # install tmuxinator
+	# gem install tmuxinator
+
+	# # install tmuxinator completion base on dev shell and dev shell rc
+}
+
+function tatSetup() {
+	# echo multiple lines into dev shell rc
+	cat >> ~/$DEVSHELLRC << EOF
+function tat {
+	name=$(basename `pwd` | sed -e 's/\.//g')
+
+	if tmux ls 2>&1 | grep "$name"; then
+		tmux attach -t "$name"
+	elif [ -f .envrc ]; then
+		direnv exec / tmux new-session -s "$name"
+	else
+		tmux new-session -s "$name"
+	fi
+}
+EOF
+}
+
+function setupAll() {
+	setupDevShell
+	setupTmux
+	setupVim
+	tatSetup
+	setupBash-it
 }
 
 function usage() {
@@ -72,6 +177,7 @@ while [ $# -gt 0 ]; do
 			identifyPackageManager
 			installPackage
 			backupDotfiles
+			setupAll
 			;;
 		-r|--recover)
 			recoverDotfiles
